0ea89b5d06ac79b1795d6f8031d62a4d4b4812e9
ftnext <takuyafjp+develop@gmail.com>
2021-08-21 18:02:02 +0900

    [feat] Add decorator for Chrome

--- connpass_ops_playbook/decorators.py
+++ connpass_ops_playbook/decorators.py
@@ -22,7 +22,22 @@ def using_firefox(func):
 
 
 def using_chrome(func):
-    raise NotImplementedError
+    """Google Chromeを使うことを示すデコレータ
+
+    Chromeを空のページで立ち上げる。
+    FIXME Heliumが固定しているSelenium (3.141.0) は Chrome version 89 のみサポートなので、
+    Chromeのバージョンが違うために起動しないケースがある
+    ref: https://github.com/mherrmann/selenium-python-helium/issues/61
+
+    注意：他のデコレータと一緒に使う場合、一番外側に置く必要がある（まずブラウザを立ち上げるため）
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_chrome()
+        func(*args, **kwargs)
+
+    return wrapper
 
 
 def logged_in(func):
