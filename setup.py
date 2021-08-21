0cf9f153367418b3043fd623626c98241a2d3ce1
ftnext <takuyafjp+develop@gmail.com>
2021-08-21 23:15:46 +0900

    [chore] MVP setup to run examples in local

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="connpass-ops-playbook",
+    version="0.0.1",
+    packages=find_packages(
+        exclude=["tests.*", "tests", "examples.*", "examples"]
+    ),
+    install_requires=["helium~=3.0.7", "chromedriver-autoinstaller"],
+    extras_require={"dev": ["black", "flake8", "pytest", "pytest-randomly"]},
+)
