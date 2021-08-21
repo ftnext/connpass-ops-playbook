f624e97dfe9df05148501c38a1e2e6ebc94d4e35
ftnext <takuyafjp+develop@gmail.com>
2021-08-21 12:52:22 +0900

    [test] Add case of unset password

--- tests/connpass_ops_playbook/test_playbooks.py
+++ tests/connpass_ops_playbook/test_playbooks.py
@@ -7,7 +7,7 @@ from connpass_ops_playbook import playbooks
 @patch("connpass_ops_playbook.playbooks.login")
 @patch("connpass_ops_playbook.playbooks.os.getenv")
 class LoginWithEnvTestCase(TestCase):
-    def test_login_success(self, getenv, login):
+    def test_when_env_vars_are_set(self, getenv, login):
         getenv.side_effect = ["the_user", "pass123"]
 
         playbooks.login_with_env()
@@ -27,4 +27,18 @@ class LoginWithEnvTestCase(TestCase):
             str(cm.exception), "Set environment variable `CONNPASS_USERNAME`"
         )
         getenv.assert_called_once_with("CONNPASS_USERNAME")
+        login.assert_not_called()
+
+    def test_error_when_password_unset(self, getenv, login):
+        getenv.side_effect = ["the_user", None]
+
+        with self.assertRaises(RuntimeError) as cm:
+            playbooks.login_with_env()
+
+        self.assertEqual(
+            str(cm.exception), "Set environment variable `CONNPASS_PASSWORD`"
+        )
+        getenv.assert_has_calls(
+            [call("CONNPASS_USERNAME"), call("CONNPASS_PASSWORD")]
+        )
         login.assert_not_called()
