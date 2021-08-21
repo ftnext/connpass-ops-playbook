4b64cdbf7e5794dab3124041f9a976783122a0f6
ftnext <takuyafjp+develop@gmail.com>
2021-08-21 12:31:21 +0900

    [test] Bundle go_to login page in login

--- tests/connpass_ops_playbook/test_plays.py
+++ tests/connpass_ops_playbook/test_plays.py
@@ -9,12 +9,14 @@ class LoginTestCase(TestCase):
     @patch("connpass_ops_playbook.plays.Text")
     @patch("connpass_ops_playbook.plays.click")
     @patch("connpass_ops_playbook.plays.write")
-    def test_login(self, write, click, Text, wait_until):
+    @patch("connpass_ops_playbook.plays.go_to")
+    def test_login(self, go_to, write, click, Text, wait_until):
         username = "the_user"
         password = "pass123"
 
         plays.login(username, password)
 
+        go_to.assert_called_once_with("connpass.com/login")
         write.assert_has_calls(
             [call(username, into="ユーザー名"), call(password, into="パスワード")]
         )
