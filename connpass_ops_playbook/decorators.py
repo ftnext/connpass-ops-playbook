8a425d710cfe28d85170f68834b17d8419cb7b7c
ftnext <takuyafjp+develop@gmail.com>
2021-08-21 17:23:02 +0900

    [feat] Add decorator to show logged in status

--- connpass_ops_playbook/decorators.py
+++ connpass_ops_playbook/decorators.py
@@ -2,6 +2,8 @@ from functools import wraps
 
 from helium import start_firefox
 
+from connpass_ops_playbook.playbooks import login_with_env
+
 
 def using_firefox(func):
     """Firefoxを使うことを示すデコレータ
@@ -12,6 +14,17 @@ def using_firefox(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         start_firefox()
+        func(*args, **kwargs)
+
+    return wrapper
+
+
+def logged_in(func):
+    """connpassにログインすることを示すデコレータ"""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        login_with_env()
         func(*args, **kwargs)
 
     return wrapper
