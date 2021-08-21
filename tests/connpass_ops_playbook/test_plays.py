e6c2273037ed1eedb6c95336d2b16d4a32faa1d9
ftnext <takuyafjp+develop@gmail.com>
2021-08-21 12:00:41 +0900

    [test] Add login play test

---
+++ tests/connpass_ops_playbook/test_plays.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from unittest.mock import call, patch
+
+from connpass_ops_playbook import plays
+
+
+class LoginTestCase(TestCase):
+    @patch("connpass_ops_playbook.plays.wait_until")
+    @patch("connpass_ops_playbook.plays.Text")
+    @patch("connpass_ops_playbook.plays.click")
+    @patch("connpass_ops_playbook.plays.write")
+    def test_login(self, write, click, Text, wait_until):
+        username = "the_user"
+        password = "pass123"
+
+        plays.login(username, password)
+
+        write.assert_has_calls(
+            [call(username, into="ユーザー名"), call(password, into="パスワード")]
+        )
+        click.assert_called_once_with("ログインする")
+        Text.assert_called_once_with("あなたのイベント")
+        wait_until.assert_called_once_with(Text.return_value.exists)
