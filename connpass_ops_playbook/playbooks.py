d1af38dc2ee71d86e88734c869ab49195cbd1a88
ftnext <takuyafjp+develop@gmail.com>
2021-08-21 12:52:53 +0900

    [feat] Raise error to unset password

--- connpass_ops_playbook/playbooks.py
+++ connpass_ops_playbook/playbooks.py
@@ -6,5 +6,6 @@ from connpass_ops_playbook.plays import login
 def login_with_env():
     if not (username := os.getenv("CONNPASS_USERNAME")):
         raise RuntimeError("Set environment variable `CONNPASS_USERNAME`")
-    password = os.getenv("CONNPASS_PASSWORD")
+    if not (password := os.getenv("CONNPASS_PASSWORD")):
+        raise RuntimeError("Set environment variable `CONNPASS_PASSWORD`")
     login(username, password)
