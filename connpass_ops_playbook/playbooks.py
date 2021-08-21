35692e773406072b8c53950f921bb6d538360bc2
ftnext <takuyafjp+develop@gmail.com>
2021-08-21 12:48:30 +0900

    [feat] Raise error when username is unset

--- connpass_ops_playbook/playbooks.py
+++ connpass_ops_playbook/playbooks.py
@@ -4,6 +4,7 @@ from connpass_ops_playbook.plays import login
 
 
 def login_with_env():
-    username = os.getenv("CONNPASS_USERNAME")
+    if not (username := os.getenv("CONNPASS_USERNAME")):
+        raise RuntimeError("Set environment variable `CONNPASS_USERNAME`")
     password = os.getenv("CONNPASS_PASSWORD")
     login(username, password)
