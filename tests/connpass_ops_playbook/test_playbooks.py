1f6ffa506bc58a9714272fc4a1716437460ba81e
ftnext <takuyafjp+develop@gmail.com>
2021-08-21 12:48:04 +0900

    [test] Add username unset case to login_with_env

--- tests/connpass_ops_playbook/test_playbooks.py
+++ tests/connpass_ops_playbook/test_playbooks.py
@@ -4,9 +4,9 @@ from unittest.mock import call, patch
 from connpass_ops_playbook import playbooks
 
 
+@patch("connpass_ops_playbook.playbooks.login")
+@patch("connpass_ops_playbook.playbooks.os.getenv")
 class LoginWithEnvTestCase(TestCase):
-    @patch("connpass_ops_playbook.playbooks.login")
-    @patch("connpass_ops_playbook.playbooks.os.getenv")
     def test_login_success(self, getenv, login):
         getenv.side_effect = ["the_user", "pass123"]
 
@@ -16,3 +16,15 @@ class LoginWithEnvTestCase(TestCase):
             [call("CONNPASS_USERNAME"), call("CONNPASS_PASSWORD")]
         )
         login.assert_called_once_with("the_user", "pass123")
+
+    def test_error_when_username_unset(self, getenv, login):
+        getenv.return_value = None
+
+        with self.assertRaises(RuntimeError) as cm:
+            playbooks.login_with_env()
+
+        self.assertEqual(
+            str(cm.exception), "Set environment variable `CONNPASS_USERNAME`"
+        )
+        getenv.assert_called_once_with("CONNPASS_USERNAME")
+        login.assert_not_called()
