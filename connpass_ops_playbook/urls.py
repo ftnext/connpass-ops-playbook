a32b1de5424e720be5e2d51867a0b99d9e92fd83
ftnext <takuyafjp+develop@gmail.com>
2021-08-22 18:38:09 +0900

    [feat] search_event_id support error case

--- connpass_ops_playbook/urls.py
+++ connpass_ops_playbook/urls.py
@@ -11,4 +11,4 @@ def to_participants_management_url(event_url):
 def search_event_id(event_url):
     parsed = urlparse(event_url)
     m = re.search(r"\d+", parsed.path)
-    return int(m[0])
+    return int(m[0]) if m is not None else None
