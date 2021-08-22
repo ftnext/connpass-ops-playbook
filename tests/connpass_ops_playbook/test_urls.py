9f5a960b1719903ba2a348e48ab128ad3a683894
ftnext <takuyafjp+develop@gmail.com>
2021-08-22 18:33:08 +0900

    [test] Split to search_event_id

--- tests/connpass_ops_playbook/test_urls.py
+++ tests/connpass_ops_playbook/test_urls.py
@@ -19,3 +19,21 @@ class ToParticipantsManagementUrlTestCase(TestCase):
         actual = urls.to_participants_management_url(event_url)
 
         self.assertEqual(actual, expected)
+
+
+class SearchEventId(TestCase):
+    def test_normal_event_url(self):
+        event_url = "https://awesome-group.connpass.com/event/1234567/"
+        expected = 1234567
+
+        actual = urls.search_event_id(event_url)
+
+        self.assertEqual(actual, expected)
+
+    def test_group_includes_numbers(self):
+        event_url = "https://event2019.connpass.com/event/2345678/"
+        expected = 2345678
+
+        actual = urls.search_event_id(event_url)
+
+        self.assertEqual(actual, expected)
