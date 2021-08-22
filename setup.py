c1210ec8278e9ee7759b9ec14faacf048cc442f2
ftnext <takuyafjp+develop@gmail.com>
2021-08-22 20:01:37 +0900

    [chore] ✍️  added dependency

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(
     packages=find_packages(
         exclude=["tests.*", "tests", "examples.*", "examples"]
     ),
-    install_requires=["helium~=3.0.7", "chromedriver-autoinstaller"],
+    install_requires=[
+        "helium~=3.0.7",
+        "chromedriver-autoinstaller",
+        "Send2Trash",
+    ],
     extras_require={"dev": ["black", "flake8", "pytest", "pytest-randomly"]},
 )
