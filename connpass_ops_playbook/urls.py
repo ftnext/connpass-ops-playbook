3df84d02650a125e48b6fc7103a2e52e4783d06e
ftnext <takuyafjp+develop@gmail.com>
2021-08-22 18:17:30 +0900

    [test] Add method to create url to download csv

---
+++ connpass_ops_playbook/urls.py
@@ -0,0 +1,2 @@
+def to_participants_management_url(event_url):
+    raise NotImplementedError
---
+++ tests/connpass_ops_playbook/test_urls.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+from connpass_ops_playbook import urls
+
+
+class ToParticipantsManagementUrlTestCase(TestCase):
+    def test_from_event_url(self):
+        event_url = "https://awesome-group.connpass.com/event/1234567/"
+        expected = "https://connpass.com/event/1234567/participants/"
+
+        actual = urls.to_participants_management_url(event_url)
+
+        self.assertEqual(actual, expected)
