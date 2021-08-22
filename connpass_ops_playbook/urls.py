ac20a9f301776f5928e1d2fc337db40550b524ed
ftnext <takuyafjp+develop@gmail.com>
2021-08-22 19:10:26 +0900

    [refactor] Remove unused method

--- connpass_ops_playbook/urls.py
+++ connpass_ops_playbook/urls.py
@@ -2,12 +2,6 @@ import re
 from urllib.parse import urlparse
 
 
-def to_participants_management_url(event_url):
-    parsed = urlparse(event_url)
-    m = re.search(r"\d+", parsed.path)
-    return f"https://connpass.com/event/{m[0]}/participants/"
-
-
 def search_event_id(event_url):
     parsed = urlparse(event_url)
     m = re.search(r"\d+", parsed.path)
--- tests/connpass_ops_playbook/test_urls.py
+++ tests/connpass_ops_playbook/test_urls.py
@@ -3,24 +3,6 @@ from unittest import TestCase
 from connpass_ops_playbook import urls
 
 
-class ToParticipantsManagementUrlTestCase(TestCase):
-    def test_normal_event_url(self):
-        event_url = "https://awesome-group.connpass.com/event/1234567/"
-        expected = "https://connpass.com/event/1234567/participants/"
-
-        actual = urls.to_participants_management_url(event_url)
-
-        self.assertEqual(actual, expected)
-
-    def test_group_includes_numbers(self):
-        event_url = "https://event2019.connpass.com/event/2345678/"
-        expected = "https://connpass.com/event/2345678/participants/"
-
-        actual = urls.to_participants_management_url(event_url)
-
-        self.assertEqual(actual, expected)
-
-
 class SearchEventId(TestCase):
     def test_normal_event_url(self):
         event_url = "https://awesome-group.connpass.com/event/1234567/"
