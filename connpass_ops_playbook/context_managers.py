d4a0529407b3c6f87174aa7e5af3be3222eb2ed9
ftnext <takuyafjp+develop@gmail.com>
2021-09-14 22:14:25 +0900

    [feat] kill browser even when error raises

--- connpass_ops_playbook/context_managers.py
+++ connpass_ops_playbook/context_managers.py
@@ -5,6 +5,12 @@ from helium import kill_browser, start_firefox
 
 @contextlib.contextmanager
 def using_firefox():
+    """Firefoxを使うコンテキストマネージャ
+
+    例外が送出された場合にも kill_browser() し、ブラウザウィンドウが残らない
+    """
     start_firefox()
-    yield
-    kill_browser()
+    try:
+        yield
+    finally:
+        kill_browser()
