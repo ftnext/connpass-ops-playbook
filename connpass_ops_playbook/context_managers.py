28f4a2c6a6f6c69486b05195daad506ab87f6d17
ftnext <takuyafjp+develop@gmail.com>
2021-09-14 21:49:18 +0900

    [test] Start firefox as context manager

---
+++ connpass_ops_playbook/context_managers.py
@@ -0,0 +1,5 @@
+from helium import kill_browser, start_firefox
+
+
+def using_firefox():
+    raise NotImplementedError
---
+++ tests/connpass_ops_playbook/test_context_managers.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+from connpass_ops_playbook import context_managers as cm
+
+
+class UsingFirefoxTestCase(TestCase):
+    @patch("connpass_ops_playbook.context_managers.kill_browser")
+    @patch("connpass_ops_playbook.context_managers.start_firefox")
+    def test_context_manager(self, start_firefox, kill_browser):
+        with cm.using_firefox():
+            ...
+
+        start_firefox.assert_called_once_with()
+        kill_browser.assert_called_once_with()
